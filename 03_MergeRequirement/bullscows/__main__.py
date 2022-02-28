c6f7f93ecda800a2f38fbfb9d48414b0c224e1dd
yura-leb <lebedinsliyyue@gmail.com>
2022-02-28 23:42:56 +0300

    Gameplay part added

--- 03_MergeRequirement/bullscows/__main__.py
+++ 03_MergeRequirement/bullscows/__main__.py
@@ -1,9 +1,28 @@
 import textdistance
+import random
+
 # Hamming, Bag
 def bullcows(guess: str, secret: str) -> (int, int):
     bag = textdistance.Bag()
     ham = textdistance.Hamming()
     bulls = len(guess) - ham(guess, secret)
     cows = bag(guess, secret)
-   return bulls, cows
+    return bulls, cows
 
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    word = random.choice(words)
+    guessed, count = False, 0
+    while not guessed:
+        ask("Введите слово: ", words)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        count += 1
+    return count
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    pass
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    pass
+
+if __name__ == "__main__":
+    pass
